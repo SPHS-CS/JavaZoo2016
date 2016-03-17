bcea180299b97be80c2404657be878a754231f0a
Harry Suh <Harryj.Suh99@gmail.com>
2016-03-17 13:09:36 -0700

    Added files via upload

---
+++ Quote2.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of interface Speak2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Quote2
+{
+    String quote2();
+}
---
+++ Quote3.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of interface Quote3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Quote3
+{
+    String quote3();
+}
