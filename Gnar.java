46f4e57e30fd3a723c95395b010ab43c6c9585ac
EdgarHR <bluehr.usa@gmail.com>
2016-03-17 08:45:20 -0700

    Added files via upload

---
+++ Gnar.java
@@ -0,0 +1,49 @@
+public class Gnar extends Animal implements Walking, Rage
+{
+    private String toy;
+    
+    public Gnar()
+    {
+        super("Gnar", "Your typical cute yordle.");
+        toy = "Boomerang";
+    }
+    
+    public Gnar(String name, String description, String toy)
+    {
+        super(name, description);
+        this.toy = toy;
+    }
+    
+    public String makeNoise()
+    {
+        return "GNARRR!";
+    }
+    
+    public String play()
+    {
+        return super.getName() + " throws his " + toy + " and runs to catch it.";
+    }
+    
+    public String eat()
+    {
+        return "It eats some .";
+    }
+    
+    @Override
+    public String walk()
+    {
+        return "Gnar runs and hides in the bush.";
+    }
+    
+    @Override
+    public String rageBar(int i)
+    {
+        if (i*2 > 100)
+        {
+            return "Gnar immediately transforms into a giant monster, he has " + 100 + " rage!!!!! RUNN!!";
+        }
+        else
+            return "Gnar is happy today, he has " + i*2 + " rage";
+    }
+}
+    
