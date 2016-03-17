6f94b0c31c0d1c1a893a069dc94428920b9d2f10
gregiscool <gregoryluck2016@gmail.com>
2016-03-17 08:35:03 -0700

    Added files via upload

---
+++ tWolf.java
@@ -0,0 +1,46 @@
+public class tWolf extends Animal implements Walking, Swimming
+{
+    private String toy;
+    
+    public tWolf()
+    {
+        super("Bear", "Just your average extinct marsupial.");
+        toy = "stuffed kangaroo";
+    }
+    
+    public tWolf(String name, String description, String toy)
+    {
+        super(name, description);
+        this.toy = toy;
+    }
+    
+    public String makeNoise()
+    {
+        return "Growl! Hey get the heck out of here!";
+    }
+    
+    public String play()
+    {
+        return "The " + super.getName() + " plays with the " + toy + " like an animal.";
+    }
+    
+    public String eat()
+    {
+        return "The " + super.getName() + " lovingly eats kangaroo meat.";
+    }
+    
+    public String drink()
+    {
+        return "The " + super.getName() + "drinks and drinks until his wife leaves him.";
+    }
+    
+    public String walk()
+    {
+        return "The " + super.getName() + " prowls across the exhibit.";
+    }
+    
+    public String swim()
+    {
+        return "The " + super.getName() + " doggy paddles across the artificial river";
+    }
+}
